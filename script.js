e34dfd6e543f3cdedcc122f6f682c7b819467977
Lila Amelia <lilaamelia77@gmail.com>
2024-04-29 21:16:13 +0700

    object initialization

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let userArr = [];
+let userCount = 0;
+let userList = document.getElementById("user-list");
 
-class Account {
+class User {
     constructor(firstName, lastName, email, password) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -13,3 +14,28 @@ class Account {
     }
 }
 
+function signUp() {
+    let firstName = (document.getElementById("first-name-input")).value;
+    let lastName = (document.getElementById("last-name-input")).value;
+    let email = (document.getElementById("email-input")).value;
+    let password = (document.getElementById("password-input")).value;
+
+    let newUser = new User(firstName, lastName, email, password);
+    userCount += 1;
+
+    localStorage.setItem("user", JSON.stringify(newUser));
+
+    console.log(newUser);
+
+    userList.innerHTML = `<li>${newUser.firstName}, ${newUser.lastName}, ${newUser.email}, ${newUser.password}</li>`; 
+
+    // displayData();
+
+}
+
+function displayData() {
+    let listParse = "";
+    listParse = JSON.parse(localStorage.getItem("user"));
+    
+    userList.innerHTML = `<li>${listParse.firstName}, ${listParse.lastName}, ${listParse.email}, ${listParse.password}</li>`; 
+}
