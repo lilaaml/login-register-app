aec4e4cfafaee12c4122b382796ae987f7136ade
Lila Amelia <lilaamelia77@gmail.com>
2024-04-29 16:51:27 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let userArr = [];
+
+class Account {
+    constructor(firstName, lastName, email, password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+    userId() {
+        let newId = (userArr.length).toString();
+        return newId;
+    }
+}
+
