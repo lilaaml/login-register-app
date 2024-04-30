ab74fd57604dd678649f1d997ef0644750244860
Lila Amelia <lilaamelia77@gmail.com>
2024-04-30 11:48:54 +0700

    clearer view

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let userCount = 0;
-let userList = document.getElementById("user-list");
+// ISSUES
+// [ ] Data cannot be displayed because it's NOT being saved globally (is inside a function instead)
 
+// Other global variables
+const userDisplay = document.getElementById("user-display"); 
+
+// Create an array
+let users = [];
+
+console.log(users);
+
+// Create a constructor
 class User {
     constructor(firstName, lastName, email, password) {
         this.firstName = firstName;
@@ -8,34 +17,44 @@ class User {
         this.email = email;
         this.password = password;
     }
-    userId() {
-        let newId = (userArr.length).toString();
-        return newId;
-    }
+    // userId() {
+    //     let newId = (userArr.length).toString();
+    //     return newId;
+    // }
 }
 
+// Create a new object and push it into the array
 function signUp() {
-    let firstName = (document.getElementById("first-name-input")).value;
-    let lastName = (document.getElementById("last-name-input")).value;
-    let email = (document.getElementById("email-input")).value;
-    let password = (document.getElementById("password-input")).value;
+    let firstName = document.getElementById("first-name-input").value;
+    let lastName = document.getElementById("last-name-input").value;
+    let email = document.getElementById("email-input").value;
+    let password = document.getElementById("password-input").value;
 
-    let newUser = new User(firstName, lastName, email, password);
-    userCount += 1;
+    let user = new User(firstName, lastName, email, password);
+    users.push(user);
 
-    localStorage.setItem("user", JSON.stringify(newUser));
+    // Clear input box
+    firstName = "";
+    lastName = "";
+    email = "";
+    password = "";
 
-    console.log(newUser);
+    // Save data
+    localStorage.setItem("users", JSON.stringify(users));
 
-    userList.innerHTML = `<li>${newUser.firstName}, ${newUser.lastName}, ${newUser.email}, ${newUser.password}</li>`; 
-
-    // displayData();
-
-}
-
-function displayData() {
-    let listParse = "";
-    listParse = JSON.parse(localStorage.getItem("user"));
+    // Destroy "User" object (make the user variable empty again)
+    user = "";
     
-    userList.innerHTML = `<li>${listParse.firstName}, ${listParse.lastName}, ${listParse.email}, ${listParse.password}</li>`; 
+    // Get the data
+    let userTemp = JSON.parse(localStorage.getItem("users"));
+    
+    // Display the data
+    let userList = document.createElement("li");
+
+    // userList.innerHTML = `<li>${newUser.firstName}, ${newUser.lastName}, ${newUser.email}, ${newUser.password}</li>`; 
 }
+
+// Push the new object into the array
+
+// checking if there's an existing user with the same email
+// if(newUser.email === user.email)
